3672c9e1855180962422e6344efbf921ce9dd64c
Manolya <201097@ppu.edu.ps>
2022-04-29 20:56:21 +0300

    in main method, I added the needed code to input the sets' sizes and took in mind the exceptions that may occure

--- sortingalg/src/sortingalg/SortingAlg.java
+++ sortingalg/src/sortingalg/SortingAlg.java
@@ -1,10 +1,26 @@
-
 package sortingalg;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class SortingAlg {
 
     public static void main(String[] args) {
-        System.out.println("testing");
-    }
+        Scanner input = new Scanner(System.in);
+//        Scanner FInput = new Scanner(file);
+        System.out.println("Enter the size of the 5 sets");
+        
+        int[] sizes = new int[5];
+        
+        try {
+            for(int i = 1; i < 6; i++){
+                System.out.print("set #"+ i + " size = ");
+                sizes[i - 1] = input.nextInt();    
+            }
+        }
+        catch(InputMismatchException | ArrayIndexOutOfBoundsException e){
+            System.out.println(e.getMessage());
+        }
     
+    }
 }
